9773f22ce3547f00be5cefa12655be6e8c4cf849
Kengkkun <waengchaikkun@gmail.com>
2021-05-21 21:55:18 +0700

    complete calculator

--- src/App.js
+++ src/App.js
@@ -1,64 +1,97 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      number1: 0,
-      number2: 0,
-      options: ['add', 'minus', 'multiply', 'divide'],
-      selectedOption: 'add',
-      result: ''
-    };
-  }
+const App = () => {
+  const [state, setState] = useState({
+    number1: 0,
+    number2: 0,
+    options: ['add', 'minus', 'multiply', 'divide'],
+    selectedOption: 'add',
+    result: 0
+  });
 
-  setNumber1 = e => {
-    this.setState({
-      number1: e.target.value,
-    })
-  }
+  const setNumber1 = e => {
+    setState({
+      ...state,
+      number1: e.target.value
+    });
+  };
 
-  setNumber2 = e => {
-    this.setState({
-      number1: e.target.value,
-    })
-  }
+  const setNumber2 = e => {
+    setState({
+      ...state,
+      number2: e.target.value
+    });
+  };
 
-  handleChange = e => {
-    this.setState({
-      selectedOption: e.target.value,
-    })
-  }
+  const handleChange = e => {
+    setState({
+      ...state,
+      selectedOption: e.target.value
+    });
+  };
 
-  getResult = e => {
-    this.setState({
-      results: this.state.number1,
-    })
-  }
+  const getResult = param => {
+    console.log(param);
+    console.log(state.number1, state.number2);
+    if (param === 'add') {
+      setState({
+        ...state,
+        results: Number(state.number1) + Number(state.number2)
+      });
+    }
+    if (param === 'minus') {
+      setState({
+        ...state,
+        results: Number(state.number1) - Number(state.number2)
+      });
+    }
+    if (param === 'multiply') {
+      setState({
+        ...state,
+        results: Number(state.number1) * Number(state.number2)
+      });
+    }
+    if (param === 'divide') {
+      setState({
+        ...state,
+        results: Number(state.number1) / Number(state.number2)
+      });
+    }
+  };
 
-  render() {
-    return (
-      <div className="App">
-        <div className="App-panel-container">
-          <div className="App-panel">
-            <input type="text" min="0" onChange={this.setNumber1}></input>
-            <br/>
-            <input type="text" min="0" onChange={this.setNumber2}></input>
+  return (
+    <div>
+      <div className='App'>
+        <div className='App-panel-container'>
+          <div className='App-panel'>
+            <input type='text' min='0' onChange={e => setNumber1(e)}></input>
+            <br />
+            <input type='text' min='0' onChange={e => setNumber2(e)}></input>
           </div>
-          <div className="App-panel">
-            <select name='selectedOption' onChange={this.handleChange} value={this.state.selectedOption}>
-              {this.state.options.map(i => (<option key={i} value={i}>{i}</option>) )}
+          <div className='App-panel'>
+            <select
+              name='selectedOption'
+              onChange={e => handleChange(e)}
+              value={state.selectedOption}
+            >
+              {state.options.map(i => (
+                <option key={i} value={i}>
+                  {i}
+                </option>
+              ))}
             </select>
-            <button onClick={this.getResult}>Get result</button>
+            <button onClick={() => getResult(state.selectedOption)}>
+              Get result
+            </button>
           </div>
-          <div className="App-panel">
-            <div>{this.state.results}</div>
+          <div className='App-panel'>
+            <div>{state.results}</div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
